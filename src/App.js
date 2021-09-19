c1473b4d99df4c07cdb3fcdffb3ecbfe949004ae
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-19 12:32:18 -0700

    uploaded login and register page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Setting from "./pages/settings/Setting";
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
-      <Write />
+      <Setting />
     </div>
   );
 }

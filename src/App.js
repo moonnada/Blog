0a193b877cb195f8025008b83ce6065b52a4cb68
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-19 15:19:52 -0700

    made to route correctly

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Setting";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SinglePost from "./components/singlePost/SinglePost";
 
 function App() {
+  const user = true;
   return (
-    <div className="App">
+    <Router>
       <Topbar/>
-      <Setting />
-    </div>
+      <Switch>
+        <Route exact path="/"> <Home /> </Route>
+        <Route path="/register"> {user ? <Home/> : <Register />} </Route>
+        <Route path="/login"> {user? <Home/> : <Login />} </Route>
+        <Route path="/write"> {user? <Write /> : <Register/> } </Route>
+        <Route path="/setting"> {user? <Setting /> : <Register />} </Route>
+        <Route path="/post/:postId"><SinglePost /></Route>
+      </Switch>
+    </Router>
   );
 }
 

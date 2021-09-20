eb2431a68e8642e99bf96116f4c8e19efad09c66
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-20 15:50:43 -0700

    updated register.js to check whether users' data is correct or not

--- react/src/pages/home/Home.js
+++ react/src/pages/home/Home.js
@@ -1,15 +1,29 @@
+import { useState, useEffect } from "react"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.css"
+import axios from "axios";
+import { useLocation } from "react-router"
 
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+    const {search} = useLocation();
+
+    useEffect(() => {
+        const fecthPosts = async () => {
+           const res = await axios.get("/posts" + search)
+            setPosts(res.data)
+        }
+        fecthPosts();
+    },[search] )
+
     return (
         <>
             <Header />
             <div className="home">
-            <Posts/>
-            <Sidebar/>
+                <Posts posts={posts}/>
+                <Sidebar/>
             </div>
         </>
     )

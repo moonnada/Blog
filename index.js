685fe45feff9160c4b338422a3843e56cbdb0cad
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-26 19:24:31 -0700

    testt

--- index.js
+++ index.js
@@ -38,12 +38,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.use(express.static(path.join(__dirname, "/react/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/react/build', 'index.html'));
-});
-
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is running");
 })
